9705484fda64d79ffae49cd7601c25f455cb10b5
Marcos Minond <minond.marcos@gmail.com>
2014-12-21 23:07:34 -0700

    putting ui build into player js code

--- index.js
+++ index.js
@@ -1,69 +1,14 @@
-var Q = require('Q'),
-    Ractive = require('ractive'),
-    Player = require('./src/player'),
+'use strict';
+
+var Player = require('./src/player'),
     Soundcloud = require('./src/soundcloud'),
-    browser = require('in-browser');
+    ui = require('./player/ui');
 
-var soundcloud = new Soundcloud('96b942eb086a5093d4ea08ab69c5a16b');
-
-var player, ractive;
+var soundcloud = new Soundcloud('96b942eb086a5093d4ea08ab69c5a16b'),
+    player = new Player(document);
 
 soundcloud.getSong('180559953').then(function (song) {
-    console.log(song);
-
-    if (browser) {
-        player.play(song);
-    }
+    player.play(song);
 });
 
-var fs = require('fs');
-
-if (browser) {
-    player = new Player(document);
-    player.nice_time = require('a-nice-time');
-
-    ractive = new Ractive({
-        el: '#player',
-        template: fs.readFileSync('./player/index.html').toString(),
-        data: player,
-        player: player,
-        play: function () {
-            this.player.audio.play();
-        },
-        pause: function () {
-            this.player.audio.pause();
-        }
-    });
-
-    player.on('play', function (song) {
-        ractive.update('song');
-    });
-
-    player.audio.addEventListener('loadstart', function (ev) {
-        console.log('loading');
-        player.audio.loading = true;
-        player.audio.canplay = false;
-    });
-
-    player.audio.addEventListener('seeking', function (ev) {
-        console.log('seeking');
-        player.audio.seeking = true;
-        ractive.update('audio');
-    });
-
-    player.audio.addEventListener('canplay', function (ev) {
-        console.log('done loading');
-        player.audio.loading = false;
-        player.audio.seeking = false;
-        player.audio.canplay = true;
-        ractive.update('audio');
-    });
-
-    player.audio.addEventListener('ended', function (ev) {
-        console.log('ended');
-    });
-
-    player.audio.addEventListener('timeupdate', function (ev) {
-        ractive.update('audio');
-    });
-}
+ui('#player', player);
---
+++ player/ui.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var fs = require('fs');
+
+var Ractive = require('ractive'),
+    nice_time = require('a-nice-time'),
+    template = fs.readFileSync('./player/index.html').toString();
+
+/**
+ * @function ui
+ * @param {String} append_to
+ * @param {Player} player
+ * @return {Ractive}
+ */
+function ui(append_to, player) {
+    var ui = new Ractive({
+        el: append_to,
+        template: template,
+        data: player,
+        play: player.audio.play.bind(player.audio),
+        pause: player.audio.pause.bind(player.audio)
+    });
+
+    player.nice_time = require('a-nice-time');
+
+    player.on('play', function (song) {
+        ui.update('song');
+    });
+
+    player.audio.addEventListener('seeking', function (ev) {
+        player.audio.seeking = true;
+        ui.update('audio');
+    });
+
+    player.audio.addEventListener('canplay', function (ev) {
+        player.audio.loading = false;
+        player.audio.seeking = false;
+        player.audio.canplay = true;
+        ui.update('audio');
+    });
+
+    player.audio.addEventListener('timeupdate', function (ev) {
+        ui.update('audio');
+    });
+
+    player.audio.addEventListener('loadstart', function (ev) {
+        player.audio.loading = true;
+        player.audio.canplay = false;
+        ui.update('song');
+    });
+
+    return ui;
+}
+
+module.exports = ui;
