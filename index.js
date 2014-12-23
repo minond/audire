0d9652963097a6a26afecb12ee33643ab7f4df26
Marcos Minond <minond.marcos@gmail.com>
2014-12-22 23:02:01 -0700

    more songs!

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var soundcloud = new Soundcloud('96b942eb086a5093d4ea08ab69c5a16b'),
 Q.all([
     soundcloud.getSong('129517248'),
     soundcloud.getSong('132883911'),
+    soundcloud.getSong('166222301'),
+    soundcloud.getSong('142744960'),
+    soundcloud.getSong('116860041'),
     soundcloud.getSong('180559953')
 ]).then(player.setPlaylist.bind(player));
 
