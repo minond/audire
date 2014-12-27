605b5fe951d0541db7f54d405f9804ea395570a7
Marcos Minond <minond.marcos@gmail.com>
2014-12-26 17:58:42 -0700

    more songs and a few style fixes/changes

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ var soundcloud = new Soundcloud('96b942eb086a5093d4ea08ab69c5a16b'),
     player = new Player(document);
 
 Q.all([
+    soundcloud.getSong('109412481'),
+    soundcloud.getSong('93866105'),
     soundcloud.getSong('129517248'),
     soundcloud.getSong('132883911'),
     soundcloud.getSong('166222301'),
     soundcloud.getSong('142744960'),
     soundcloud.getSong('116860041'),
-    soundcloud.getSong('180559953')
+    soundcloud.getSong('180559953'),
+    soundcloud.getSong('154798838'),
+    soundcloud.getSong('115780423'),
+    soundcloud.getSong('165098282'),
 ]).then(player.setPlaylist.bind(player));
 
 require('./player/styles.less');
